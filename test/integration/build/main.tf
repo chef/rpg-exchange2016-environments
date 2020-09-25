cad3587cb4f045e9ce85527243dc4878739d5018
Dylan Martin <dmartin@chef.io>
2020-09-25 14:41:56 +0100

    AZ change
    Signed-off-by: Dylan Martin <dmartin@chef.io>

--- test/integration/build/main.tf
+++ test/integration/build/main.tf
@@ -37,7 +37,7 @@ resource "aws_cloudformation_stack" "exchange2016" {
   name = "exchange-stack"
 
   parameters = {
-    AvailabilityZones = "eu-north-1a,eu-north-1b",
+    AvailabilityZones = "us-west-2a,us-west-2b",
     ADServer1NetBIOSName = "DC1",
     ADServer1InstanceType = "t3.large"
     ADServer2InstanceType = "t3.large"
